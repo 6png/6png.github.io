3c5a4fb2c20663b9ca950f17d54f409b3a337145
6png <renawangb@gmail.com>
2024-02-10 14:50:22 -0800

    style(Thoughts): improve design flow of elements on thought page

--- src/components/Thoughts/Thoughts.js
+++ src/components/Thoughts/Thoughts.js
@@ -117,18 +117,24 @@ const Thoughts = () => {
                             d="m60 600c0 298.8 241.2 540 540 540s540-241.2 540-540c0-297.6-241.2-540-540-540s-540 242.4-540 540zm523.2-253.2c22.801 22.801 22.801 57.602 0 80.398-1.1992 2.3984-116.4 116.4-116.4 116.4h404.4c31.199 0 56.398 25.199 56.398 56.398 0 31.199-25.199 57.602-56.398 57.602l-404.4 0.003906 116.4 116.4c22.801 22.801 22.801 58.801 0 80.398-22.801 22.801-57.602 22.801-80.398 0-4.8008-4.8008-213.6-213.6-213.6-213.6-22.801-22.801-22.801-57.602 0-80.398 0 0 212.4-212.4 213.6-213.6 22.801-22.797 57.598-22.797 80.398 0.003906z"/>
                     </svg>
                 </button>
-                <h2>{thoughtToRender.fields.title}</h2>
-                <hr></hr>
-                <div className="timeDetails">
-                    <p>published: {formatDate(thoughtToRender.sys.createdAt)}</p>
-                    <p>last updated: {formatDate(thoughtToRender.sys.updatedAt)}</p>
+                <div className="thoughtContentHeader">
+                    <h2>{thoughtToRender.fields.title}</h2>
                 </div>
-                <hr className="thinHr"></hr>
+                <hr className="doubleHr1"></hr>
                 <div className="thoughtContentBody">
                     <RichTextComponent
                         key={thoughtToRender.sys.id}
                         richText={thoughtToRender.fields.content}
                         assets={assets}/>
+                </div>
+                <hr className="doubleHr2"></hr>
+                <div className="thoughtContentFooter">
+                    <div className="timeDetails">
+                        <p>
+                            (is) posted {formatDate(thoughtToRender.sys.createdAt)}pt |
+                            last updated {formatDate(thoughtToRender.sys.updatedAt)}pt
+                        </p>
+                    </div>
                 </div>
             </div>
         )
@@ -138,7 +144,8 @@ const Thoughts = () => {
         <div className="bodyWrapper">
             <div className="side-menu">
                 <nav>
-                    <button className={selectedContent === 'pinned' ? 'highlighted' : ''} onClick={() => handleSidebarClick('pinned')}>pinned</button>
+                    <button className={selectedContent === 'pinned' ? 'highlighted' : ''}
+                            onClick={() => handleSidebarClick('pinned')}>pinned</button>
                     <button className={selectedContent === 'all' ? 'highlighted' : ''} onClick={() => handleSidebarClick('all')}>all</button>
                 </nav>
             </div>
