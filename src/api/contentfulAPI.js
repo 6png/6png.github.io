d64f84e52235bb5731b8292fec0f17a95b7605e5
6png <renawangb@gmail.com>
2024-02-14 13:54:08 -0800

    feat(Thoughts): display youtube links as embeds

--- src/api/contentfulAPI.js
+++ src/api/contentfulAPI.js
@@ -1,5 +1,5 @@
 import * as contentful from 'contentful';
-import {BLOCKS} from "@contentful/rich-text-types";
+import {INLINES, BLOCKS} from "@contentful/rich-text-types";
 import React from "react";
 
 const client = contentful.createClient({
@@ -18,7 +18,34 @@ export const renderOptions = {
                     alt={node.data.target.fields.description}
                 />
             )
-        }
+        },
+        [INLINES.HYPERLINK]: node => {
+            console.log(node);
+            if (node.data.uri.includes("youtube.com")) {
+                const match = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/.exec(
+                    node.data.uri
+                )
+                const videoId =
+                    match && match[7].length === 11 ? match[7] : null
+                return (
+                    videoId && (
+                        <section className="video-container">
+                            <iframe
+                                className="video"
+                                title={`https://youtube.com/embed/${videoId}`}
+                                src={`https://youtube.com/embed/${videoId}`}
+                                allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
+                                allowFullScreen
+                            />
+                        </section>
+                    )
+                )
+            } else {
+                return (
+                    <a href={node.data.uri}>{node.content[0].value}</a>
+                )
+            }
+        },
     }
 }
 
